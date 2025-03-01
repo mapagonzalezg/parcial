d46b7a128b2a29ca791b3cf81dd7897738d7f620
mapagonzalezg <mpgg1903@gmail.com>
2025-02-28 19:27:39 -0500

    v.0.3

--- program.js
+++ program.js
@@ -1,5 +1,18 @@
-var map = L.map('map').setView([4.666728091118099, -74.12012717159114], 16);
+var map = L.map('map').setView([4.669450147376564, -74.11759330312128], 16);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+// fetch es para obtener algo, await es funcion asincronica
+async function loadPolygon(){
+    let myData= await fetch('modelia.geojson');
+    let myPolygon= await myData.json();
+    L.geoJSON(myPolygon,
+        {
+            style:{
+                color:"blue"
+            }
+        }
+    ).addTo(map);
+}
+loadPolygon();
